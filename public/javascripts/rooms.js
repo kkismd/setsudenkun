5c66fc6870e63295fd2f008aa20b38b9b1743934
SHIMADA Keiki <kshimada@appirits.com>
2014-08-22 17:07:00 +0900

    put on icon and emit message and save to mongodb

--- public/javascripts/rooms.js
+++ public/javascripts/rooms.js
@@ -1,24 +1,79 @@
 $(function () {
-    var socket, uid, roomId;
-    $('#btn').on('click', function (ev) {
-        socket.emit('set icon',
-            {uid: uid,
-                roomId: roomId,
-                x: ev.clientX,
-                y: ev.clientY
-            });
-    });
-
+    var uid, roomId;
+    var socket = io();
     socket.on('hello', function (data) {
         uid = data.uid;
         roomId = data.roomId;
         console.log("uid: " + uid + ", roomId: " + roomId);
     });
-
     socket.on('members', function (data) {
         uid = data.uid;
         roomId = data.roomId;
         console.log('uid: ' + data.uid);
         console.log('members: ' + data.gTmembers);
+    });
+    (function () {
+        var $main = $('#main');
+        var $shot = $('#shot');
+        var $scold = $('#scold');
+        var n1 = $shot.clone(true).removeClass('proto');
+        $main.append(n1);
+        n1.css({ top: 237, left: 222 });
+
+        var n2 = $shot.clone(true).removeClass('proto');
+        $main.append(n2);
+        n2.css({ top: 371, left: 481 });
+
+        var n3 = $shot.clone(true).removeClass('proto');
+        $main.append(n3);
+        n3.css({ top: 316, left: 169 });
+
+        var n4 = $scold.clone(true).removeClass('proto');
+        $main.append(n4);
+        n4.css({ top: 560, left: 201 });
+    })();
+
+    // GUIの状態を表す変数
+    var INIT = 0,
+        GRAB = 1,
+        PUT = 2;
+    var guiState = INIT;
+
+    var $hotIcon = $('#hot');
+    var $main = $('#main');
+    var offset = $main.offset();
+    function getPos(pageX, pageY) {
+        var x = pageX - offset.left - 35 + 100;
+        var y = pageY - offset.top - 35;
+        return {top: y, left: x};
+    }
+
+    $(document).on('mousemove.move', function (ev) {
+        if (guiState == GRAB) {
+            $hotIcon.css(getPos(ev.pageX, ev.pageY));
+        }
+    });
+
+    $hotIcon.on('click', function (ev) {
+        if (guiState == INIT) {
+            guiState = GRAB;
+        } else if (guiState == GRAB) {
+            // アイコンを置いた
+            var pos = getPos(ev.pageX, ev.pageY);
+            socket.emit('set icon', {
+                uid: uid,
+                roomId: roomId,
+                x: pos.left,
+                y: pos.top
+            });
+            guiState = PUT;
+        } else if (guiState == PUT) {
+            // アイコンを拾った
+            socket.emit('remove icon', {
+                uid: uid,
+                roomId: roomId
+            });
+            guiState = GRAB;
+        }
     });
 });
